5ceee4ca409ecd3b2c18a9aeaeeab3473c1020e0
Mike Lester <themikelester@gmail.com>
2016-12-17 23:41:17 -0800

    Can query based on lat/lng. Added Google Map point selection. Select Day to Day+2 via buttons

--- index.js
+++ index.js
@@ -6,24 +6,23 @@ var app = express()
 app.use(express.static('public'))
 
 app.get('/test', function (req, res) {
-	sounding.QuerySounding(function(err, json) {
-		if( err ) throw err;
-		res.send(json);
-	});
-})
-
-app.get('/skewt', function (req, res) {
 	sounding.QuerySoundingFile('data/denver.gsd', function(err, json) {
 		if( err ) throw err;
 		res.send(json);
 	});
 })
 
+app.get('/getSounding', function (req, res) {
+	console.log('SkewT Request');
+	sounding.QuerySounding(req.query.latLong, req.query.timeRange, req.query.model, function(err, json) {
+		if( err ) {
+			res.status(500).send(err.message);
+		} else {
+			res.send(json);
+		}
+	});
+})
+
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!')
 })
-
-// sounding.QuerySoundingFile('data/denver.gsd', function(err, json) {
-// 		if( err ) throw err;
-// 		console.log(json);
-// 	});
--- src/sounding.js
+++ src/sounding.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var fs = require('fs');
-
+const assert = require('assert');
 var Parser = require('./parser');
+
+const verbose = false;
 
 function ParseSounding(text) {
 	var parser = new Parser();
@@ -9,8 +11,33 @@ function ParseSounding(text) {
 	return jsonData
 }
 
-var QuerySounding = function(onComplete) {
-	var request = { host:'rucsoundings.noaa.gov', path:'/get_soundings.cgi?start=latest&' }
+// Arguments are passed according to http://rucsoundings.noaa.gov/text_sounding_query_parameters.pdf
+var QuerySounding = function(latLong, timeRangeEpoch, model, onComplete) {
+	var args = '/get_soundings.cgi?';
+
+	if( verbose ) { console.log('QuerySounding called: ' + arguments); }
+
+	// Verify Latitude and Longitude coordinates. N and E being positive, separated by a comma. E.g. '40.1,­105.6'.
+	var coords = [];
+	try { coords = latLong.split(',').map(Number); } catch(e) { return onComplete(new Error( "latLong must be an array containing 2 numbers")); }
+	if(!(coords[0] < 90 && coords[0] > -90)) { return onComplete(new Error("Invalid latitude")); }
+	if(!(coords[1] < 180 && coords[1] > -180)) { return onComplete(new Error("Invalid longitude")); }
+	args += 'airport=' + coords[0] + ',' + coords[1] + '&';
+
+	// Verify start range. Must be comma separated, start and end are in epoch time (UTC time since 1/1/1970) in seconds.
+	if( timeRangeEpoch ) {
+		var timeRange = []	
+		try { timeRange = timeRangeEpoch.split(',').map(Number); } catch(e) { return onComplete(new Error( "timeRangeEpoch must be an array containing 2 numbers")); }
+		// @TODO: verify that the start time is not too far in the past
+		// @TODO: verify that the time range is not too long for the given model
+		args += 'startSecs=' + timeRange[0] + '&';
+		args += 'endSecs=' + timeRange[1] + '&';
+	} else {
+		args += 'start=latest&';
+	}
+
+	if( verbose ) { console.log( 'rucsoundings.noaa.gov' + args ); }
+	var request = { host:'rucsoundings.noaa.gov', path:args }
 	http.request(request, function(response) {
 		var text = '';
 		
@@ -19,6 +46,7 @@ var QuerySounding = function(onComplete) {
 		});
 
 		response.on('end', function () {
+			if( verbose ) { console.log( 'Response status: ' + response.statusCode ) }
 			var json = ParseSounding( text );
 			onComplete(null, json)
 		});
