df859708d3404f178a6410594424cd40b526daa0
Mike Lester <themikelester@gmail.com>
2017-01-28 16:30:32 -0800

    Switched from port 3000 to 80

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get('/getSounding', function (req, res) {
 	});
 })
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+	console.log('Example app listening on port 80!')
 })
